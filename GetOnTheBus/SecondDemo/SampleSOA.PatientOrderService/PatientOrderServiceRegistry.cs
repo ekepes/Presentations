f4fb7f7e9ea9c446a74edbc689e41ff3e58e8aa6
Eric Kepes <eric.kepes@mckesson.com>
2012-05-02 13:32:55 -0400

    Second demo.

---
+++ GetOnTheBus/SecondDemo/SampleSOA.PatientOrderService/PatientOrderServiceRegistry.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+
+using Ninject.Modules;
+
+namespace SampleSOA.PatientOrderService
+{
+    public class PatientOrderServiceRegistry :
+        NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ItemPickedHandler>().ToSelf();
+
+            Bind<PatientOrderService>()
+                .To<PatientOrderService>()
+                .InSingletonScope();
+
+            Bind<IServiceBus>().ToMethod(context => ServiceBusFactory.New(sbc =>
+                {
+                    sbc.UseRabbitMqRouting();
+                    sbc.ReceiveFrom("rabbitmq://localhost/patient_order_service");
+                }))
+                .InSingletonScope();
+        }
+    }
+}
